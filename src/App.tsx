537e5bb51434d57e7fa80c70fd356c7b8a1f5955
Kipras <kipraskb@gmail.com>
2025-06-18 15:41:42 +0300

    added logo and created initial navbar structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styles from "./App.module.css";
 import { Navbar } from "./components/navbar/Navbar";
 import { Home } from "./Pages/Home";
-import { TestPage } from "./Pages/TestPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </Router>
+    <div>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import logo from "../../assests/logo.png";
 
 export const Navbar = () => {
-  return <div className={styles.test}>test</div>;
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className={styles.buttons}></div>
+      </div>
+    </nav>
+  );
 };
